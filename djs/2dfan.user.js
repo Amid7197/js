e537abb6d9b442deaef6381ef892e7b402d2ee96
Amid7197 <harustein39@gmail.com>
2025-12-09 22:55:49 +0800

    Update 2dfan.user.js

--- djs/2dfan.user.js
+++ djs/2dfan.user.js
@@ -2,7 +2,7 @@
 // @name         2dfan跳转
 // @namespace    http://tampermonkey.net/
 // @version      1.0.2
-// @description  多彩渐变搜索按钮
+// @description  多彩渐变搜索按钮1.0.2
 // @author       cuddly
 // @match        *://ddfan.*/*
 // @match        *://2dfan.*/*
